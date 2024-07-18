7ac40083945b22bde9c1c0ac5572995d7f9d4f40
loranfd <andrealoranfdez@gmail.com>
2024-07-18 13:21:44 +0200

    funcion nuevo alumno

--- modelos/bbdd/datos.php
+++ modelos/bbdd/datos.php
@@ -7,12 +7,17 @@ function listado_alumnos(){
     $array = $alumnado->fetchAll(PDO::FETCH_ASSOC);
     return($array);
 }
-function nuevo_alumno($i){ //me da pereza meter todo, está función está totalmente sin hacer
+function nuevo_alumno_prueba($i){ //me da pereza meter todo, está función está totalmente sin hacer. holi!! no borro esto por si la mía está mal o falla en algún momento//
     $mbd = new PDO('mysql:host='.SERVIDOR_BBDD.';dbname='.BBDD, USER_BBDD, PASSWORD_BBDD); 
     $sql = "INSERT INTO datos (in) VALUES (?,?,?,?,?,?) ";
     $mbd->prepare($sql)->execute([$i]);
 }
 
+function nuevo_alumno($apellidos, $nombre, $fecha_atencion, $asistencia, $tipo_asistencia, $tipo_ausencia, $sexo, $prestacion, $nivel_estudios, $edad, $carnet, $vehiculo, $entrevista, $tipo_atencion, $situacion, $caso_ocupado, $discapacidad) {
+    $mbd = new PDO('mysql:host='.SERVIDOR_BBDD.';dbname='.BBDD, USER_BBDD, PASSWORD_BBDD);
+    $sql = "INSERT INTO alumnado (apellidos, nombre, fecha_atencion, asistencia, info_asistencia, info_ausencia, sexo, prestacion, nivel_estudios, edad, carnet, vehiculo, tipo_entrevista, tipo_atencion, situacion, ocupado, discapacidad) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+     $mbd->prepare($sql)->execute([$apellidos, $nombre, $fecha_atencion, $asistencia, $tipo_asistencia, $tipo_ausencia, $sexo, $prestacion, $nivel_estudios, $edad, $carnet, $vehiculo, $entrevista, $tipo_atencion, $situacion, $caso_ocupado, $discapacidad]);    
+}
 
 
 //esto es lo que usé de prueba para la prueba de las gráficas, por ahora no quiero borrarlo hasta que consiga hacer las gráficas reales.
