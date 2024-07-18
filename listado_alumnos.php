cb703943a7957b778e170ab67898c722aa3d7d05
judipm <judipm25@gmail.com>
2024-07-18 11:04:01 +0200

    estilos y listado ya centrado
    creé estilos.css y lo vinculé a la cabecera para que esté vinculado para todas las páginas.
    Ya está la tabla centrada en el medio. Mejor guardo y actualizo y me pongo con el funcionamiento de los filtros

--- listado_alumnos.php
+++ listado_alumnos.php
@@ -1,24 +1,54 @@
+<?php 
+include "modelos/bbdd/datos.php";
+$listado_alumnos = listado_alumnos();
+foreach($listado_alumnos as $alumno){
+?>
+
 <script>
-    function elegirfilm(num){
+    var tabla = "<tr><th>Apellidos</th><th>Nombre</th><th>Sexo</th><th>Edad</th><th>Situación actual</th><td></td></tr>";
+    tabla =+ "<tr>";
+    tabla =+ "<td>" + <?= $alumno["Apellidos"]; ?> + "</td>";
+    tabla =+ "<td>" + <?= $alumno["Nombre"]; ?> + "</td>";
+    tabla =+ "<td>" + <?= $alumno["Sexo"]; ?> + "</td>";
+    tabla =+ "<td>" + <?= $alumno["Situacion"]; ?> + "</td>";
+    tabla =+ "</tr>";                
+
+    document.getElementById("listado").innerHTML = tabla;
+    
+function situacion_laboral(num){
     switch(num){
         case 1:
-            document.getElementById("listado").innerHTML = '<img src="img/sociedad.jpg" width="350"/>';
+            document.getElementById("listado").innerHTML = $alumnado["Situacion"] == "Desempleado";
             break;
         case 2:
-            document.getElementById("listado").innerHTML = '<img src="img/titanic.jpg" width="350"/>';
+            document.getElementById("listado").innerHTML = $alumnado["Situacion"] == "Ocupado" ;
+    }   
+}
+function sexo(num){
+    switch(num){
+        case 1:
+            document.getElementById("listado").innerHTML = $alumnado["Sexo"] == "Mujer";
+            break;
+        case 2:
+            document.getElementById("listado").innerHTML = $alumnado["Sexo"] == "Hombre";
+            
+    }   
+}
+function edad(num){
+    switch(num){
+        case 1:
+            document.getElementById("listado").innerHTML = $alumnado["Edad"] < 35;
+            break;
+        case 2:
+            document.getElementById("listado").innerHTML = $alumnado["Edad"] < 60 && $alumnado["Edad"] >= 35;
             break;
         case 3:
-            document.getElementById("listado").innerHTML = '<img src="img/abremejaime.jpg" width="350"/>';
-            break;
-        case 4:
-            document.getElementById("listado").innerHTML = '<img src="img/it.jpg" width="350"/>';
+            document.getElementById("listado").innerHTML = $alumnado["Edad"] >= 60;
     }   
 }
 </script>
+
 <?php 
-include "modelos/bbdd/datos.php";
-
-$listado_alumnos = listado_alumnos();
-
+}
 include "vistas/listado_alumnos.html";
 ?>
