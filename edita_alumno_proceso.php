32a1343115d1262f0cb1c6227bcab43b7062077a
loranfd <andrealoranfdez@gmail.com>
2024-07-22 12:52:50 +0200

    no acabado

--- edita_alumno_proceso.php
+++ edita_alumno_proceso.php
@@ -1,6 +1,6 @@
 <?php 
 
-	
+
 	include "modelos/bbdd/usuario.php";
 	
 	actualizar_usuario($_POST['apellidos'], $_POST['nombre'], $_POST['fecha_atencion'], $_POST['asistencia'], $_POST['tipo_asistencia'], $_POST['tipo_ausencia'], $_POST['sexo'], $_POST['prestacion'], $_POST['nivel_estudios'], $_POST['edad'], $_POST['carnet'], $_POST['vehiculo'], $_POST['entrevista'], $_POST['tipo_atencion'], $_POST['situacion'], $_POST['caso_ocupado'], $_POST['discapacidad']);
