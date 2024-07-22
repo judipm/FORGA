975dc5ae622e4a75a1a70ee9df7c9bcd92a1a0a1
loranfd <andrealoranfdez@gmail.com>
2024-07-22 12:49:00 +0200

    Funciones pertinentes para editar usuario

--- modelos/bbdd/datos.php
+++ modelos/bbdd/datos.php
@@ -18,7 +18,20 @@ function nuevo_alumno($apellidos, $nombre, $fecha_atencion, $asistencia, $tipo_a
     $sql = "INSERT INTO alumnado (apellidos, nombre, fecha_atencion, asistencia, info_asistencia, info_ausencia, sexo, prestacion, nivel_estudios, edad, carnet, vehiculo, tipo_entrevista, tipo_atencion, situacion, ocupado, discapacidad) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
      $mbd->prepare($sql)->execute([$apellidos, $nombre, $fecha_atencion, $asistencia, $tipo_asistencia, $tipo_ausencia, $sexo, $prestacion, $nivel_estudios, $edad, $carnet, $vehiculo, $entrevista, $tipo_atencion, $situacion, $caso_ocupado, $discapacidad]);    
 }
+function actualizar_alumno ($id, $apellidos, $nombre, $fecha_atencion, $asistencia, $tipo_asistencia, $tipo_ausencia, $sexo, $prestacion, $nivel_estudios, $edad, $carnet, $vehiculo, $entrevista, $tipo_atencion, $situacion, $caso_ocupado, $discapacidad){
+    $mbd = new PDO('mysql:host='.SERVIDOR_BBDD.';dbname='.BBDD, USER_BBDD, PASSWORD_BBDD);
+    $sql = "UPDATE alumnado SET apellidos =?, nombre =?, fecha_atencion =?, asistencia =?, info_asistencia =?, info_ausencia =?, sexo =?, prestacion =?, nivel_estudios =?, edad =?, carnet =?, vehiculo =?, tipo_entrevista =?, tipo_atencion =?, situacion =?, ocupado =?, discapacidad =? WHERE id =?";
+    $mbd->prepare($sql)->execute([$apellidos, $nombre, $fecha_atencion, $asistencia, $tipo_asistencia, $tipo_ausencia, $sexo, $prestacion, $nivel_estudios, $edad, $carnet, $vehiculo, $entrevista, $tipo_atencion, $situacion, $caso_ocupado, $discapacidad, $id]);
+}
 
+function datos_un_usuario($id){	
+	
+		$mbd = new PDO('mysql:host='.SERVIDOR_BBDD.';dbname='.BBDD, USER_BBDD, PASSWORD_BBDD);
+		$sql = "SELECT * FROM alumnado WHERE id='" . $id ."'";
+		$usuarios = $mbd->query($sql);
+		$usuario = $usuarios->fetch(PDO::FETCH_ASSOC);
+		return $usuario;
+    }
 
 //esto es lo que usé de prueba para la prueba de las gráficas, por ahora no quiero borrarlo hasta que consiga hacer las gráficas reales.
 function listado(){
