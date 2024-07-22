5b6cfce44a83784846710576ea47cd458cf63396
judipm <judipm25@gmail.com>
2024-07-22 20:37:19 +0200

    filtros funcionan

--- listado_alumnos_filtrado.php
+++ listado_alumnos_filtrado.php
@@ -6,7 +6,7 @@ $listado_alumnos = listado_alumnos();
 $situacion_laboral = $_POST["situacion"];
 $sexo = $_POST["sexo"];
 $edad = $_POST["edad"];
-echo $situacion_laboral. "<br>". $sexo. "<br>". $edad;
+    
 include "vistas/listado_alumnos_filtrado.html";
 
 ?>
