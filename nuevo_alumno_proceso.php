876329ea1f2fa05b7f9a25eed08b7e643ddb2b58
judipm <judipm25@gmail.com>
2024-07-22 14:47:41 +0200

    Arreglado tipo de carnet
    Usé implode() para convertir el array en un string
    tambien cambié el header para que redirigiera a la pagina del listado al acabar

--- nuevo_alumno_proceso.php
+++ nuevo_alumno_proceso.php
@@ -1,8 +1,9 @@
 <?php
 
 include "modelos/bbdd/datos.php";
+$carnet = implode(",", $_POST['carnet']);
 
-nuevo_alumno($_POST['apellidos'], $_POST['nombre'], $_POST['fecha_atencion'], $_POST['asistencia'], $_POST['tipo_asistencia'], $_POST['tipo_ausencia'], $_POST['sexo'], $_POST['prestacion'], $_POST['nivel_estudios'], $_POST['edad'], $_POST['carnet'], $_POST['vehiculo'], $_POST['entrevista'], $_POST['tipo_atencion'], $_POST['situacion'], $_POST['caso_ocupado'], $_POST['discapacidad']);
+nuevo_alumno($_POST['apellidos'], $_POST['nombre'], $_POST['fecha_atencion'], $_POST['asistencia'], $_POST['tipo_asistencia'], $_POST['tipo_ausencia'], $_POST['sexo'], $_POST['prestacion'], $_POST['nivel_estudios'], $_POST['edad'], $carnet, $_POST['vehiculo'], $_POST['entrevista'], $_POST['tipo_atencion'], $_POST['situacion'], $_POST['caso_ocupado'], $_POST['discapacidad']);
 
-header('location: index.php');
+header('location: listado_alumnos.php');
 ?>
